f38569e7cdd88d8b323fc7b7e2020b502169ec1c
Shubham <shubhamsutariya21k@gmail.com>
2024-12-25 15:46:13 +0530

    FEATURE | sitemap+contact+privacy-policy page UI

--- app/_components/CoursesSection.tsx
+++ app/_components/CoursesSection.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import cardImage1 from '../../lib/assets/images/card_image_1.png'
 import { IconStarFilled } from "@tabler/icons-react";
 import Rating from "@/lib/components/custom/ratting";
+import { getPosts } from "@/lib/web-api/post/get-posts";
 
 const courses = [
     {
@@ -32,8 +33,9 @@ const courses = [
     },
 ];
 
-export const CoursesSection = () => (
-    <Box className="section_Divider" bg='theme-orange.0'>
+export const CoursesSection = async () => {
+    const posts = await getPosts();
+    return < Box className="section_Divider" bg='theme-orange.0' >
         <Container size="xl" >
             <Title c='theme-black.2' fw={400} order={3} lh={{ base: rem(20), md: rem(50) }}>
                 Help mensen beter in contact te komen met zichzelf en anderen . <br />
@@ -41,19 +43,19 @@ export const CoursesSection = () => (
             </Title>
 
             <Grid gutter={{ base: "xl", md: "lg", lg: "36" }} >
-                {courses.map((course, index) => (
+                {posts.splice(0,3).map((course, index) => (
                     <GridCol span={{ base: 12, lg: 4 }} key={index} >
                         <Title c="theme-black.2" mb='sm' order={3} lineClamp={2} mih={rem(80)}>{course.title}</Title>
-                        <Text c="theme-black.2" mb='sm' size="16px">{course.subTitle}</Text>
+                        <Text c="theme-black.2" mb='sm' size="16px">{course.title}</Text>
                         <Flex align='center' gap='sm' mb='sm'>
                             <Rating rating={5} />
-                            <Text c="theme-black.2" size="16px">{course.reviews} reviews</Text>
+                            <Text c="theme-black.2" size="16px">25 reviews</Text>
                         </Flex>
-                        <Image src={course.image} alt={course.title} height={160} width={355} />
+                        <Image src={cardImage1} alt={course.title} height={160} width={355} />
                         <br />
-                        <Text c="theme-black.2" lh={{ base: rem(20), md: rem(24) }} mih={rem(72)}>{course.description}</Text>
+                        <Text c="theme-black.2" lh={{ base: rem(20), md: rem(24) }} mih={rem(72)}>{course.body}</Text>
                         <br />
-                        <Text size="sm" c="theme-orange.1">Eerst start: {course.startDate}</Text>
+                        <Text size="sm" c="theme-orange.1">Eerst start: 12-16-2025</Text>
                         <br />
                         <Button color="theme-blue" w='auto'>Bekijk opleiding</Button>
                     </GridCol>
@@ -61,4 +63,4 @@ export const CoursesSection = () => (
             </Grid>
         </Container>
     </Box >
-);
+};
